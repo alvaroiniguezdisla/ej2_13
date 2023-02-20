b1d1faf1593bd72d64b201d75f65163a6074a99a
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 16:03:34 +0100

    ej13 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,10 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.ej2_13;
+
+public class Principal {
+    public static void main(String[] args) {
+        double num;
+        System.out.println(ej2_13.equals(num=(1+Math.sqrt(5)/2)));
+
+    }
 }
--- src/dominio/ej2_13.java
+++ src/dominio/ej2_13.java
@@ -1,2 +1,35 @@
-package dominio;public class ej2_13 {
+package dominio;
+
+public class ej2_13 {
+
+
+    public static int fib13(int n) {
+        if(n<2) {
+            return n;
+        }else {
+            return fib13(n-1)+fib13(n-2);
+        }
+
+    }
+    public static int fib12(int n) {
+        if(n<2) {
+            return n;
+        }else {
+            return fib12(n-1)+fib12(n-2);
+        }
+
+    }
+
+    public static boolean equals(double num) {
+        double n13=fib13(13);
+        double n12=fib13(12);
+        double cociente=n13/n12;
+        if(num==cociente) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
 }
+
